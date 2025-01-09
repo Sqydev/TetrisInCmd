308f48ab557e72eaf7dd809821f8ed2ecb91ef10
sqyd <wojkap10@proton.me>
2025-01-09 18:25:16 +0100

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   TetrisInCmd.sln 	new file:   TetrisInCmd/Program.cs 	new file:   TetrisInCmd/TetrisInCmd.csproj 	new file:   TetrisInCmd/TetrisInCmd.sln 	new file:   TetrisInCmd/bin/Debug/net8.0/System.CommandLine.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/TetrisInCmd 	new file:   TetrisInCmd/bin/Debug/net8.0/TetrisInCmd.deps.json 	new file:   TetrisInCmd/bin/Debug/net8.0/TetrisInCmd.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/TetrisInCmd.pdb 	new file:   TetrisInCmd/bin/Debug/net8.0/TetrisInCmd.runtimeconfig.json 	new file:   TetrisInCmd/bin/Debug/net8.0/cs/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/de/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/es/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/fr/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/it/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/ja/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/ko/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/pl/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/pt-BR/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/ru/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/tr/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/zh-Hans/System.CommandLine.resources.dll 	new file:   TetrisInCmd/bin/Debug/net8.0/zh-Hant/System.CommandLine.resources.dll 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.AssemblyInfo.cs 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.AssemblyInfoInputs.cache 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.GeneratedMSBuildEditorConfig.editorconfig 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.GlobalUsings.g.cs 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.assets.cache 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.csproj.AssemblyReference.cache 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.csproj.CopyComplete 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.csproj.CoreCompileInputs.cache 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.csproj.FileListAbsolute.txt 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.dll 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.genruntimeconfig.cache 	new file:   TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.pdb 	new file:   TetrisInCmd/obj/Debug/net8.0/apphost 	new file:   TetrisInCmd/obj/Debug/net8.0/ref/TetrisInCmd.dll 	new file:   TetrisInCmd/obj/Debug/net8.0/refint/TetrisInCmd.dll 	new file:   TetrisInCmd/obj/TetrisInCmd.csproj.nuget.dgspec.json     new file:   TetrisInCmd/obj/TetrisInCmd.csproj.nuget.g.props 	new file:   TetrisInCmd/obj/TetrisInCmd.csproj.nuget.g.targets 	new file:   TetrisInCmd/obj/project.assets.json 	new file:   TetrisInCmd/obj/project.nuget.cache

---
+++ TetrisInCmd/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program{
+    static void Main(){
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Black;
+
+
+        string menuimg = "qwewqeqweqwe";
+        
+        Console.WriteLine(menuimg);
+    }
+}
