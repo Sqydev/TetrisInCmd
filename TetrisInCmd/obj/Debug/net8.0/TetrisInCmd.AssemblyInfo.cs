d3599c96ccf7eae622a32d3bd21929c92c76d72a
sqyd <wojkap10@proton.me>
2025-01-10 21:15:47 +0100

    Reppaited Menu text, added readkey to make program go next v2

--- TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.AssemblyInfo.cs
+++ TetrisInCmd/obj/Debug/net8.0/TetrisInCmd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TetrisInCmd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2191a0a706196cacf1c70dccbfa4d36c0fa1bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d5faf53644213678f2ea2c71b7597021b303012")]
 [assembly: System.Reflection.AssemblyProductAttribute("TetrisInCmd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TetrisInCmd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
