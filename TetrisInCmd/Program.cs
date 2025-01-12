8700da821196b0ee17f1f0d94725fe80fb12109c
_Sqyd_ <138494523+Sqydev@users.noreply.github.com>
2025-01-13 00:34:25 +0100

    Update Program.cs
    Na telefonie, do sprawdzenia czy działa

--- TetrisInCmd/Program.cs
+++ TetrisInCmd/Program.cs
@@ -1,13 +1,17 @@
-﻿using System;
+using System;
+using System.Media; //zimportuj
 
 class Program{
     static void Main(){
         Console.Clear();
         Console.BackgroundColor = ConsoleColor.Black;
         Console.ForegroundColor = ConsoleColor.White;
-        
 
         Console.WriteLine(Assets.menuimg);
+
+        SoundPlayer gamemusic = new SoundPlayer("TetrisMusic.wav"); // zweryfikuj czy dobrze napisałeś
+        gamemusic.Load();
+        gamemusic.PlayLooping();//sprawdz
 
         Console.ReadKey();
     }
