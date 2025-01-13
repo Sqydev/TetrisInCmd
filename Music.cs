8c411c8c31c390be0545b304a1baad143d60a2fc
sqyd <wojkap10@proton.me>
2025-01-13 15:59:27 +0100

    added music

--- Music.cs
+++ Music.cs
@@ -7,17 +7,18 @@ public static class Music {
 
     public static void MusicPlayer() {
         if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-            SoundPlayer gamemusic = new SoundPlayer("Music/Theme.wav");
+            SoundPlayer gamemusic = new SoundPlayer("Music/Theme.mp3");
             gamemusic.Load();
             gamemusic.PlayLooping();
         }
         else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
             Core.Initialize();
-            using (var libVLC = new LibVLC())
-            using (var mediaPlayer = new MediaPlayer(libVLC)) {
-                var media = new Media(libVLC, "Music/Theme.wav", FromType.FromLocation);
-                mediaPlayer.Play(media);
-            }
+            using var libVLC = new LibVLC();
+            using var mediaPlayer = new MediaPlayer(libVLC);
+            var media = new Media(libVLC, "/home/sqyd/projekty/C#/TetrisInCmd/Music/Theme.mp3", FromType.FromPath);
+            mediaPlayer.Play(media);
+            mediaPlayer.EndReached += (sender, e) => mediaPlayer.Play(media);
+            Console.ReadLine();
         }
     }
 }
