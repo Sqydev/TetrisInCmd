94be617e70758e150d2ee23f982ae0ff13d6104a
sqyd <wojkap10@proton.me>
2025-01-13 18:50:52 +0100

    repaired music

--- Music.cs
+++ Music.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Media;
 using System.Runtime.InteropServices;
 using LibVLCSharp.Shared;
@@ -6,19 +5,21 @@ using LibVLCSharp.Shared;
 public static class Music {
 
     public static void MusicPlayer() {
-        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-            SoundPlayer gamemusic = new SoundPlayer("Music/Theme.wav");
-            gamemusic.Load();
-            gamemusic.PlayLooping();
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-            Core.Initialize();
-            using var libVLC = new LibVLC();
-            using var mediaPlayer = new MediaPlayer(libVLC);
-            var media = new Media(libVLC, "/home/sqyd/projekty/C#/TetrisInCmd/Music/Theme.mp3", FromType.FromPath);
-            mediaPlayer.Play(media);
-            mediaPlayer.EndReached += (sender, e) => mediaPlayer.Play(media);
-            Console.ReadLine();
-        }
+    string musicPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Music", "Theme.mp3");
+
+    if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+        SoundPlayer gamemusic = new SoundPlayer(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Music", "Theme.wav"));
+        gamemusic.Load();
+        gamemusic.PlayLooping();
     }
+    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+        Core.Initialize();
+        using var libVLC = new LibVLC();
+        using var mediaPlayer = new MediaPlayer(libVLC);
+        var media = new Media(libVLC, musicPath, FromType.FromPath);
+        mediaPlayer.Play(media);
+        mediaPlayer.EndReached += (sender, e) => mediaPlayer.Play(media);
+        Console.ReadLine();
+    }
+}
 }
